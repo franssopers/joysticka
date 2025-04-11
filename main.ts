3bf047df0570f5c5fefd7915e04ffc6461e16656
franssopers <50528638+franssopers@users.noreply.github.com>
2025-04-11 13:37:31 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+joystickbit.initJoystickBit()
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) <= 200) {
+        basic.showArrow(ArrowNames.East)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) <= 200) {
+        basic.showArrow(ArrowNames.South)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) >= 800) {
+        basic.showArrow(ArrowNames.West)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) >= 800) {
+        basic.showArrow(ArrowNames.North)
+    }
+})
